9f5ab77c9febee60734006633d3de145990d9cb2
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-03-20 00:07:45 +0000

    Add support for fetching all GCP projects.

--- Rakefile
+++ Rakefile
@@ -4,10 +4,13 @@ require 'oj'
 Oj.mimic_JSON
 
 require_relative 'lib/recommendations'
+require_relative 'lib/project'
 
 desc 'Generate right sizing data in JSON'
 task :generate_right_sizing_data, [:file_path] do |_, args|
   args.with_defaults(file_path: 'data/right_sizing_data.json')
-  recommendations = Recommendations.new(ENV['GCP_COOKIE'])
-  recommendations.for('')
+  # recommendations = Recommendations.new(ENV['GCP_COOKIE'])
+  # recommendations.for('foo')
+
+  p Project.new.fetch_all
 end
---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'googleauth'
+require 'google/apis/cloudresourcemanager_v1'
+
+class Project
+  def initialize
+    @service = Google::Apis::CloudresourcemanagerV1::CloudResourceManagerService.new
+    @service.authorization = Google::Auth.get_application_default(['https://www.googleapis.com/auth/cloud-platform'])
+  end
+
+  def fetch_all
+    @service.fetch_all(items: :projects) do |token|
+      @service.list_projects(page_token: token)
+    end.to_a
+  end
+end
